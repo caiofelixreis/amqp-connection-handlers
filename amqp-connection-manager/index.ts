82b40ea8b3b0b2d38880531e7235fc57c8198d65
ofelix <caiofelix.reis@gmail.com>
2024-10-14 21:21:03 -0300

    feat: initial commit

---
+++ amqp-connection-manager/index.ts
@@ -0,0 +1,45 @@
+import amqp from "amqp-connection-manager";
+import { IAmqpConnectionManager } from "amqp-connection-manager/dist/types/AmqpConnectionManager";
+
+export class AmqpConnectionManager {
+  connection: IAmqpConnectionManager;
+
+  constructor() {
+    this.connection = this.connect();
+  }
+
+  private connect(): IAmqpConnectionManager {
+    const conn = amqp.connect(["amqp://user:password@localhost:5672"], {
+      heartbeatIntervalInSeconds: 5,
+      reconnectTimeInSeconds: 1,
+    });
+
+    conn.addListener("connect", (data: any) => {
+      // Emitted whenever we successfully connect to a broker.
+      console.log("✅ - Connected");
+    });
+    conn.addListener("connectFailed", (data: any) => {
+      // Emitted whenever we attempt to connect to a broker, but fail.
+      console.log("🌀 - Connection failed");
+    });
+    conn.addListener("disconnect", (data: any) => {
+      // Emitted whenever we disconnect from a broker.
+      console.log("🚩 - Disconnected");
+    });
+
+    return conn;
+  }
+
+  async publish(queue: string, message: Record<string, any>) {
+    const channelWrapper = this.connection.createChannel({
+      json: true,
+      setup: (channel: any) => {
+        return channel.assertQueue(queue, { durable: true });
+      },
+    });
+
+    await channelWrapper.sendToQueue(queue, message);
+
+    console.log("published message", new Date().toTimeString());
+  }
+}
---
+++ index.ts
@@ -0,0 +1,13 @@
+import { AmqpConnectionManager } from "./amqp-connection-manager";
+
+function main() {
+  const amqpConnectionManager = new AmqpConnectionManager();
+
+  setInterval(async () => {
+    await amqpConnectionManager.publish("test-queue", {
+      message: "Hello World",
+    });
+  }, 1000);
+}
+
+main();
